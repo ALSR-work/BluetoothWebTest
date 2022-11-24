97049db62b3dedf750f1366f47c2c185ec1c21bd
DESKTOP-2JH1AUF\BU-VA-Steuerungstech <alexander.seeger@schmalz.de>
2022-11-24 08:09:51 +0100

    no message

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -245,13 +245,8 @@ class BluetoothTerminal {
       filters: [{services: [this._serviceUuid]}],
     }).
     */
-   /*
     return navigator.bluetooth.requestDevice({
       acceptAllDevices: true,
-    }).
-    */
-    return navigator.bluetooth.requestDevice({
-      filters: [{name: "Prüfvorrichtung"}],
     }).
         then((device) => {
           this._log('"' + device.name + '" bluetooth device selected');
