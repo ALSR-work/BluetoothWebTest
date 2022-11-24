89ae5e834a62e0e16d414581e73fb3ba90d3fcf7
DESKTOP-2JH1AUF\BU-VA-Steuerungstech <alexander.seeger@schmalz.de>
2022-11-24 08:43:16 +0100

    Filteränderung

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -251,7 +251,7 @@ class BluetoothTerminal {
     }).
     */
     return navigator.bluetooth.requestDevice({
-      filters: [{services: ["19b10001-e8f2-537e-4f6c-d104768a1214"]}],
+      filters: [{optionalServices: ["19b10001-e8f2-537e-4f6c-d104768a1214"]}],
     }).
         then((device) => {
           this._log('"' + device.name + '" bluetooth device selected');
